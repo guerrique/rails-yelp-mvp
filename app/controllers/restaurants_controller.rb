e99337d638b60b973a57756d8d4156e6804e661a
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-05-15 14:57:12 +0200

    form rendering stuff

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,8 +14,11 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant.id)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      render :new
+    end
   end
 
   def edit
