5672fb47021e8e1d238e5351d5bb9e1b3ee54d06
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-05-15 14:51:25 +0200

    totally forgot what I did already

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Coretta',
+    address:      '151 bis, rue Cardinet - 75017 Paris',
+    phone_number: '01 42 26 55 55',
+    category:     'french'
+  },
+  {
+    name:         "L' Office",
+    address:      '3, rue Richer - 75009 Paris',
+    phone_number: '01 47 70 67 31',
+    category:     'french'
+  },
+  {
+    name:         'Le Pantruche',
+    address:      '3, rue Victor Massé - 75009 Paris',
+    phone_number: '01 48 78 55 60',
+    category:     'french'
+  },
+   {
+    name:         'Zébulon',
+    address:      '10, rue Richelieu - 75001 Paris',
+    phone_number: '01 42 36 49 44',
+    category:     'japanese'
+  },
+  {
+    name:         'Pirouette',
+    address:      '5, rue Mondétour - 75001 Paris',
+    phone_number: '01 40 26 47 81',
+    category:     'belgian'
+  },
+  {
+    name:         'Bouillon',
+    address:      '47, rue de Rochechouart - 75009 Paris',
+    phone_number: '09 51 18 66 59',
+    category:     'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
