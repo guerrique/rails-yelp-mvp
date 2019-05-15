e93a969d185602d71703f9b470515225081ee93e
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-05-15 16:09:50 +0200

    oke-la

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,17 +7,21 @@ class ReviewsController < ApplicationController
 
   def create
     restaurant = Restaurant.find(params[:restaurant_id])
-    review = Review.new(review_params)
+    @review = Review.new(review_params)
+    @review.restaurant = restaurant
 
-    review.restaurant = restaurant
-    review.save
+    if @review.save
+      redirect_to restaurant_path(restaurant)
+    else
+      render :new
+    end
 
-    redirect_to restaurant_path(restaurant)
+    # redirect_to restaurant_path(restaurant)
   end
 
   private
 
-  def user_params
+  def review_params
     params.require(:review).permit(:content, :rating)
   end
 end
